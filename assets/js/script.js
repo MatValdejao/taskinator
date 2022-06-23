d3ca67a1e4dc7230ea066ea9a75e6231ff961fd4
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-06-22 22:38:42 -0400

    loadTasks()

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,8 +75,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
     //loop through tasks array and task object with new content
     for (var i=0; i < tasks.length; i++) {
         if (tasks[i].id === parseInt(taskId)) {
-            task[i].name = taskName;
-            task[i].type = taskType;
+            tasks[i].name = taskName;
+            tasks[i].type = taskType;
         }
     }
 
@@ -191,11 +191,45 @@ var editTask = function(taskId) {
     document.querySelector("select[name='task-type']").value = taskType;
     document.querySelector("#save-task").textContent = "Save Task";
     formEl.setAttribute("data-task-id", taskId);
-
 }
 
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+var loadTasks = function() {
+    tasks = localStorage.getItem("tasks", tasks);
+    if (tasks === null) {
+        tasks = [];
+        return false;
+    }
+    tasks = JSON.parse(tasks);
+    for (var i=0; i < tasks.length; i++) {
+        tasks[i].id = taskIdCounter;
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+        listItemEl.setAttribute("data-type-id", tasks[i].id);
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+        listItemEl.appendChild(taskInfoEl);
+        var taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+        if (tasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "completed") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        } 
+        taskIdCounter++;
+    }
+    
 }
 
 formEl.addEventListener("submit", taskFormHandler);
